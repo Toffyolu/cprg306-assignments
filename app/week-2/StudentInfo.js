2126b275853d14c19921d7da105f5c828b4c4ada
Tofunmi Olu <olutimehintofunmi@gmail.com>
2026-01-23 04:24:38 -0700

    Week 2 setup

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Tofunmi Olutimehin</p>
+      <Link href="https://github.com/Toffyolu/cprg306-assignments">
+        GitHub Repository
+      </Link>
+    </div>
+  );
+}
